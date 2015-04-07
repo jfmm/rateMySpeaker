663be037bab335c8fa081788f8016c235c4d0f35
jfmm <moralejf@gmail.com>
2015-04-07 15:05:39 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,22 @@
+var numOfEvalutaions = parseInt(prompt("How many evaluations were turned in?"));
+var numOfSpeakers = parseInt(prompt("How many speakers were in this program?"));
+var speakerList = [];
+
+
+for(var i = 1; i <= numOfSpeakers; i++ ) {
+  
+  var speaker = prompt("Enter in speaker #" + i);
+  
+  speakerList.push(speaker);
+
+}
+
+
+
+// nested for loop gathers all the data and computes the average
+
+
+
+
+
+
