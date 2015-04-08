cb659844ddab41f84abef9a3729356ee9ebf521a
jfmm <moralejf@gmail.com>
2015-04-08 16:41:58 -0400

    succesfully implements for loop to create JSON object

--- js/app.js
+++ js/app.js
@@ -1,68 +1,81 @@
 // first ask for parameters
 var numOfEvalutaions = parseInt(prompt("How many evaluations were turned in?")),
-    numOfSpeakers = parseInt(prompt("How many speakers were in this program?"));
+    numOfSpeeches = parseInt(prompt("How many speeches in this program?"));
 
 
-
-var speakerList = [];
 
 // Ask for speakers names
-for(var i = 1; i <= numOfSpeakers; i++ ) {
+var speechList = [];
+
+for(var i = 1; i <= numOfSpeeches; i++ ) {
   
-  var speaker = prompt("Enter in speaker #" + i);
-  speakerList.push(speaker);
+  var speaker = prompt("Enter speaker's name of speech #" + i);
+  speechList.push(speaker);
 
 }
 
 
 
 var ratings = [];
-var ratingsJSON;
+var evaluationData = {};
 
-// nested for loop gathers all the data and computes the average
 for(var i = 1; i <= numOfEvalutaions; i++) {
   
-  var currentEvalNum = i;
-
-  //TODO: loop only stores last rating right now
-  for(var ii = 0; ii < speakerList.length; ii++) {
-    
-    var currentSpeaker = speakerList[ii];
-    var rating = parseInt(prompt("Enter rating for " + currentSpeaker));
-
-
-    ratingsJSON = {
-      evaluationNum: i,
-      speakerName: currentSpeaker,
-      speakerRating: rating
-    };
-    
-    ratings.push(ratingsJSON); 
-        
-   
+  // create evaluation nested object
+  evaluationData["eval_" + i] = {}; 
+  
  
-    }//ends inner for
-
+  for(var ii = 1; ii <= numOfSpeeches; ii++) {
     
+      // create speech nested object
+      evaluationData["eval_" + i]["speech_" + ii] = {};
+      
+      // get rating data
+      var currentSpeaker = speechList[ii - 1];
+      var rating = parseInt(prompt("What did " + currentSpeaker + " get for eval #" + i));
+
+      evaluationData["eval_" + i]["speech_" + ii].speaker_name = currentSpeaker;
+      evaluationData["eval_" + i]["speech_" + ii].rating = rating;
+      
+  
+    }//ends inner for
 
   } //end outer for
 
+  
+
+//for(var evaluation in evaluationData) {
+//
+//  
+//  
+//  // this is how you access the damn object... duh
+//  //console.log(evaluationData[evaluation].speech_1.rating);
+//  
+//}
 
 
 
-//IDEAL JSON OBJECT
+
+
+
+/* separate
+==============================================*/
+
+//IDEAL if it could be turned to this JSON OBJECT
 var ideal = {
   
   speaker1: {
     name: "Bill Thomas",
     1: 5,
-    2: 4
+    2: 4,
+    3: 7
   },
     
   speaker2: {
     name: "Walter Olezek",
     1: 7,
-    2: 6
+    2: 6,
+    3: 2
   }
 
 };
@@ -86,8 +99,9 @@ for (var spkr in ideal) {
     
     // compute average
     sum += ideal[spkr][evalEntry];
-    avg = sum / numOfEvalutaions;
+    avg = (sum / numOfEvalutaions).toFixed(2);
     
+
     // just output the average once all the computations have been done
     if(evalEntry == Object.keys(ideal[spkr]).length - 1)
       console.log(ideal[spkr].name + " average: " + avg);
