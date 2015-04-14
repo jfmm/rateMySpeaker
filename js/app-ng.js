8584e93253ddceb576a1d7ae278ea3cc78277224
Jose Morales-mendizabal <moralejf@gmail.com>
2015-04-14 13:24:30 -0400

    updates UI and two way data binding

--- js/app-ng.js
+++ js/app-ng.js
@@ -1,40 +1,34 @@
 angular.module('rateMySpeaker', [])
   .controller('rate', function ($scope) {
 
+  function averageRating (total, numOfEvals) {
+    return total/numOfEvals;
+  }
+  
   
   $scope.ratingData = {
+    
+    "numOfEvaluations" : 1,
     
     "speech_1" : {
       "speaker_name": "Bill Thomas",
       "total_score" : 0,
-      "average_score" : 0
+      "average_score" : 8
     },
     "speech_2" : {
       "speaker_name": "Hisham Melhem",
-      "total_score" : 10,
+      "total_score" : 0,
       "average_score" : 0
     },
      "speech_3" : {
       "speaker_name": "Martin Russo",
-      "total_score" : 2,
+      "total_score" :0,
       "average_score" : 0
     },
   
   };
   
+  //$scope.ratingData = {};
   
-  
-  
-  
-  /*
-  * Adds total
-  **/
-  //$scope.totalScore = 0;
-  
-  $scope.add = function(e) {
-   
-  //$scope.ratingData.total_score += parseInt(e.toElement.dataset.value);
-   
-  }
   
   });
