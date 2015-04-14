9cc2f0d82bdebe10f3637ad72693d8df85c6917f
Jose Morales-mendizabal <moralejf@gmail.com>
2015-04-14 19:44:45 -0400

    refactors way in which the object is populated via data binding

--- js/app-ng.js
+++ js/app-ng.js
@@ -1,34 +1,25 @@
 angular.module('rateMySpeaker', [])
   .controller('rate', function ($scope) {
+  
 
-  function averageRating (total, numOfEvals) {
-    return total/numOfEvals;
+  
+  $scope.numOfEvaluations = 0;
+  $scope.numOfSpeeches = 0;
+  
+  
+  
+  
+  $scope.ratingData = [];
+  
+  $scope.addSpeech = function() {
+    
+    $scope.ratingData.push({
+      "speaker_name": "New Speaker",
+      "total_score" : 0,
+      "average_score" : 0
+    });
+
   }
   
-  
-  $scope.ratingData = {
-    
-    "numOfEvaluations" : 1,
-    
-    "speech_1" : {
-      "speaker_name": "Bill Thomas",
-      "total_score" : 0,
-      "average_score" : 8
-    },
-    "speech_2" : {
-      "speaker_name": "Hisham Melhem",
-      "total_score" : 0,
-      "average_score" : 0
-    },
-     "speech_3" : {
-      "speaker_name": "Martin Russo",
-      "total_score" :0,
-      "average_score" : 0
-    },
-  
-  };
-  
-  //$scope.ratingData = {};
-  
-  
-  });
+
+}); // end of module
